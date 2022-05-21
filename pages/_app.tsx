2ae13f47a00ff39c7b24e0cbd375534fea4757c7
themaxopti <themaxopti@gmail.com>
2022-05-21 18:07:39 +0300

    deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,13 +20,11 @@ const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
             .then((ReactPixel) => {
                 ReactPixel.init('428428595603693') //  428428595603693
                 ReactPixel.pageView()
-
                 router.events.on('routeChangeComplete', () => {
                     ReactPixel.pageView()
                 })
             })
     }, [router.events])
-
 
     return (
         <>
@@ -38,6 +36,24 @@ const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
             <LocalizationProvider locale={ruLocale} dateAdapter={AdapterDateFns}>
                 <Component {...pageProps} />
             </LocalizationProvider>
+
+
+            <Script id="facebook-pixel">
+                {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '428428595603693');
+      fbq('track', 'PageView');
+      fbq('track', 'CompleteRegistration');
+      fbq('track', 'Purchase', {value: 0.00, currency: 'USD'});
+      `}
+            </Script>
         </>
     )
 }
